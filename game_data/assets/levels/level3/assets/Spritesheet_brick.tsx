657d39d41b30a2b7f09d1e8b0455bb098070bd09
hehehexdd <everydaystuff42069@gmail.com>
2021-06-19 15:24:35 +0300

    changed sprites for level3

---
+++ game_data/assets/levels/level3/assets/Spritesheet_brick.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Spritesheet_brick" tilewidth="16" tileheight="16" tilecount="24" columns="8">
+ <image source="Spritesheet_brick.png" width="128" height="48"/>
+</tileset>
