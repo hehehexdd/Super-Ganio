c8250b58b3d02524136537b70bbbf7b0450fe82b
hehehexdd <everydaystuff42069@gmail.com>
2021-06-19 12:40:49 +0300

    minor bugfix on level1, added level3

---
+++ game_data/assets/levels/level3/assets/Spritesheet_dirt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Spritesheet_dirt" tilewidth="16" tileheight="16" tilecount="8" columns="4">
+ <image source="../../level2/assets/Spritesheet_dirt.png" width="64" height="32"/>
+</tileset>
