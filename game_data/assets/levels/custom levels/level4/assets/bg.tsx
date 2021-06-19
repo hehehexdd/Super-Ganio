5a4833db5df7da0cdc58918b723ea5dc57f05cc9
hehehexdd <everydaystuff42069@gmail.com>
2021-06-19 15:49:11 +0300

    added a custom level to load

---
+++ game_data/assets/levels/custom levels/level4/assets/bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="bg" tilewidth="32" tileheight="32" tilecount="3600" columns="80">
+ <image source="bg.png" width="2560" height="1440"/>
+</tileset>
